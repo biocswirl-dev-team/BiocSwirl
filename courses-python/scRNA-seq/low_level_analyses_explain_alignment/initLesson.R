77cfc3b71d0ea58d2e0dd49315523b2f8e259f6b
bensonlee <bensonlee5@gmail.com>
2020-05-22 12:41:22 -0700

    upd

---
+++ courses-python/scRNA-seq/low_level_analyses_explain_alignment/initLesson.R
@@ -0,0 +1,10 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+.get_course_path <- function(){
+	tryCatch(swirl:::swirl_courses_dir(),
+					 error = function(c) {file.path(find.package("swirl"),"Courses")}
+	)
+}
