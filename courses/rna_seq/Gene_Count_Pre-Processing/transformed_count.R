8f95e7459e8f4745ded5ec43e56e8d80845992c6
Julia Philipp <julia@Julias-MacBook-Pro.local>
2020-05-23 19:27:10 -0700

    finish pca lesson, extended deseq2 lesson, added RData files

--- courses/rna_seq/Gene_Count_Pre-Processing/transformed_count.R
+++ courses/rna_seq/Gene_Count_Pre-Processing/transformed_count.R
@@ -1,11 +1,11 @@
 # compare raw and transformed count data
 df <- bind_rows(
-  as_data_frame((counts(dds, normalized=FALSE))[,1:2]) %>% mutate(transformation ="raw"),
-  as_data_frame(assay(rlog_dds)[, 1:2]) %>% mutate(transformation = "rlog"))
+  data.frame((counts(dds, normalized=FALSE))[,1:2]) %>% mutate(transformation ="raw"),
+  data.frame(assay(rlog_dds)[, 1:2]) %>% mutate(transformation = "rlog"))
 colnames(df)[1:2] <- c("x", "y")  
 lvls <- c("raw", "rlog")
 df$transformation <- factor(df$transformation, levels=lvls)
-par(mfrow=c(1,2))
+
 plot1 <- ggplot((subset(df, transformation == "raw")), aes(x=x, y=y)) +
   geom_hex(bins = 80) +
   ggtitle("raw counts")
@@ -14,4 +14,5 @@ plot2 <- ggplot(subset(df, transformation == "rlog"), aes(x=x, y=y)) +
   geom_hex(bins = 80) + 
   ggtitle("rlog transformed counts")
 
-plot_grid(plot1, plot2)
+print(plot_grid(plot1, plot2))
+
