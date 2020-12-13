39f4c5b7cef16ad36c0e52c5889a2fdf79b507e2
Julia Philipp <julia@Julias-MacBook-Pro.local>
2020-12-13 12:12:23 -0800

    change course directory structure. add metafiles for r package

---
+++ BiocSwirlpkg/R/load_courses.R
@@ -0,0 +1,7 @@
+# load_course function
+
+load_course <- function(){
+  install_course_github(github_username = "biocswirl-dev-team",
+                        course_name = "BiocSwirl/courses/mbb343",
+                        branch = "mbb343")
+}
