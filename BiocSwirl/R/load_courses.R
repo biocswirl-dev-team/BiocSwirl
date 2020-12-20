5f30069fadbb817b7e655c0c098b4101fed363bf
Julia Philipp <julia@Julias-MacBook-Pro.local>
2020-12-19 17:08:32 -0800

    add function for warnings

--- BiocSwirl/R/load_courses.R
+++ BiocSwirl/R/load_courses.R
@@ -67,3 +67,64 @@ load_all <- function(){
   message("all courses installed successfully!")
 }
 
+
+# load courses with warning
+#' This function will load and install all available BiocSwirl courses for you
+#' @description  and create warnings (for demo purposes) because I was too scared to include this in the other function.
+#' @export
+#' @param course name of the course that could produce a warning like 'mbb343' if already installed
+#' @examples
+#' load_courses_warning('mbb343')
+load_courses_warning <- function(course) {
+  if (!course %in% course_directory$course_name){
+    stop(paste(course,"is not a valid course name. Use list_courses() to check for available courses."))
+  }
+
+  # look up string in table to install appropriate course
+  selected_course <- course_directory[which(course_directory$course_name == course),]
+
+  out <- tryCatch(
+    {
+      # Just to highlight: if you want to use more than one
+      # R expression in the "try" part then you'll have to
+      # use curly brackets.
+      # 'tryCatch()' will return the last evaluated expression
+      # in case the "try" part was completed successfully
+
+      #message("This is the 'try' part")
+
+      swirl::install_course_github(github_username = selected_course$github_username,
+                                   course_name = selected_course$course_name,
+                                   branch = selected_course$branch)
+      # The return value of `readLines()` is the actual value
+      # that will be returned in case there is no condition
+      # (e.g. warning or error).
+      # You don't need to state the return value via `return()` as code
+      # in the "try" part is not wrapped inside a function (unlike that
+      # for the condition handlers for warnings and error below)
+    },
+    error=function(cond) {
+      message(paste("course does not seem to exist:", course))
+      message("Here's the original error message:")
+      message(cond)
+      # Choose a return value in case of error
+      return(NA)
+    },
+    warning=function(cond) {
+      warning(paste("course", course, "already installed"))
+      # Choose a return value in case of warning
+      return(NULL)
+    },
+    finally={
+      # NOTE:
+      # Here goes everything that should be executed at the end,
+      # regardless of success or error.
+      # If you want more than one expression to be executed, then you
+      # need to wrap them in curly brackets ({...}); otherwise you could
+      # just have written 'finally=<expression>'
+      #message(paste("Processed URL:", course))
+      #message("Some other message at the end")
+    }
+  )
+  return(out)
+}
