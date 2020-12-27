19c32f69cbe09af0e0aa0b1ca90da1f833b3952f
Julia Philipp <julia@Julias-MacBook-Pro.local>
2020-12-26 21:51:34 -0800

    moved files to topdir

---
+++ doc/biocswirl.R
@@ -0,0 +1,27 @@
+## ---- eval = FALSE------------------------------------------------------------
+#  install.packages("devtools")
+#  library(devtools)
+#  devtools::install_github("biocswirl-dev-team/BiocSwirl")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  # list all available BiocSwirl courses
+#  list_courses()
+#  
+#  # install selected course.
+#  # copy name from output of list_courses()
+#  load_course(course ='RNAseq')
+
+## ---- eval = FALSE------------------------------------------------------------
+#  start_course()
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  # list all installed BiocSwirl courses
+#  installed_courses()
+#  
+#  # uninstall selected course
+#  uninstall_course(course = 'RNAseq')
+#  
+#  # uninstall all BiocSwirl courses
+#  # this will not uninstall other swirl courses
+#  uninstall_all_courses()
+
