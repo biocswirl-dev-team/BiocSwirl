3a5f65f256741d92cdf2be5f30522aab5b120988
Julia Philipp <julia@Julias-MacBook-Pro.local>
2020-12-29 11:03:36 -0800

    add release course install info

--- R/course_directory.R
+++ R/course_directory.R
@@ -1,8 +1,8 @@
 ## course directory
 # contains all the install info
 # this would be the only data that needs to be up to date
-course_directory <- data.frame(course_name = c("Intro_to_Data_Science","RNAseq"),
-                               course_full = c("BiocSwirl_Intro_to_Data_Science","BiocSwirl_RNAseq"),
-                               github_username = c("biocswirl-dev-team", "biocswirl-dev-team"),
-                               branch = c("main","main"),
-                               description = c("Intro to R and data analysis","bulk RNAseq analysis"))
+course_directory <- data.frame(course_name = c("Intro_to_Data_Science","Intro_to_Stats", "RNAseq","scRNAseq","ChIPseq"),
+                               course_full = c("BiocSwirl_Intro_to_Data_Science","BiocSwirl_Intro_to_Stats", "BiocSwirl_RNAseq", "BiocSwirl_scRNAseq", "BiocSwirl_ChIPseq"),
+                               github_username = rep("biocswirl-dev-team", 5),
+                               branch = rep("main", 5),
+                               description = c("Intro to R and data analysis","Intro to Statistics", "bulk RNAseq analysis","single-cell RNAseq analysis","ChIPseq analysis"))
